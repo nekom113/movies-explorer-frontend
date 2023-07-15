9938b8cfe00f14bab6f2b5db3902027bedfad0d6
Alex Piontek <nekom113@mail.ru>
2023-07-15 12:36:13 +0300

    add validation component

---
+++ src/Hooks/useValidationForm.js
@@ -0,0 +1,41 @@
+import {useCallback, useState} from "react";
+
+export default function useValidationForm() {
+    const [inputValue, setInputValue] = useState({});
+    const [inputError, setInputErrors] = useState({});
+    const [validationStatus, setValidationStatus] = useState(false);
+    const [currentNameInput, setCurrentNameInput] = useState('');
+
+    const handleChangeForm = ({target}) => {
+        const {name, value, validationMessage, closest} = target;
+        setInputValue({
+            ...inputValue,
+            [name]: value
+        });
+        setInputErrors({
+            ...inputError,
+            [name]: validationMessage
+        });
+        setValidationStatus(closest('form').checkValidity());
+        setCurrentNameInput(name)
+    }
+    const resetForm = useCallback(() => {
+            setInputValue({})
+            setInputErrors({})
+            setValidationStatus(false)
+        }, [inputValue,
+            inputError,
+            validationStatus]
+    )
+
+    return (
+        {
+            inputValue,
+            validationStatus,
+            inputError,
+            currentNameInput,
+            resetForm,
+            handleChangeForm,
+        }
+    )
+};
